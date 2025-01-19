98a8cd75af4c4b39d3bb863c60b96e01047fe574
Jonasgabs <jonas.sarmento@academico.ifpb.edu.br>
2025-01-19 12:58:50 -0300

    update: pagina about e responsividade. header e footer como components

---
+++ src/Components/footer.js
@@ -0,0 +1,14 @@
+export function Footer(){
+    return`
+    <footer>
+      <p class="margem">WhaleQuiz</p>
+      <article class="conteiner2">
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-linkedin-in"></i>
+          <i class="fa-brands fa-facebook"></i>
+          <i class="fa-brands fa-twitter"></i>
+      </article>
+      <p class="margem"> &copy; IFPB | Paulo Antônio - Jonas Gabriel - Victor Belfort</p>
+    </footer>
+    `;
+}
