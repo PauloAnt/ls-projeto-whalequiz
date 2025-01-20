7e05cf5ae2c206c2edbf7bbbeb4d2764ac994b9a
Jonasgabs <jonas.sarmento@academico.ifpb.edu.br>
2025-01-20 16:14:19 -0300

    create: card componente, update: cor login e registro

---
+++ src/Components/card.js
@@ -0,0 +1,14 @@
+export function card(){
+    return`
+        <a href="#">
+            <div class="card">
+                <img class="card-img" src="img/CardImage.png" alt="imagem do card">
+                <dl>
+                    <dt class="card-name">QUIZ 1</dt>
+                    <br>
+                    <dd>Descricao do quiz</dd>
+                </dl>
+            </div>
+        </a>
+    `;
+}
