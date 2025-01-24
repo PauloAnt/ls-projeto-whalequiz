c5f8ac9a0b4bad104c447f479e6d35165d95ef2c
PauloAnt <pauloabf235@gmail.com>
2025-01-24 11:31:43 -0300

    create&update: Correção dos imports css, Adição de link e rotas e criação do cardHistorico

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,15 +3,15 @@ export default function Header(){
    
         <nav>
             <ul class="conteiner1">
-                <li class="logo"><a href="#"  data-rota="home"><img class="whale-logo link" src="src/Images/logo.jpg" width="50px" height="50px" alt="WhaleQuiz"></a></li>
+                <li class="logo"><a href="#" class="link" data-rota="home"><img class="whale-logo link" src="src/Images/logo.jpg" width="50px" height="50px" alt="WhaleQuiz"></a></li>
                 <li class="criar">
-                    <a href="#"><i class="fa-solid fa-plus"></i>Criar</a>
+                    <a href="#" class="link" data-rota="quizCriar"><i class="fa-solid fa-plus"></i>Criar</a>
                 </li>
                 <li class="pesquisar">
                     <input type="text" name="pesquisar-quiz" id="pesquisar-quiz" placeholder="Pesquisar Quiz">
                 </li>
                 <li class="entrar">
-                    <a href="#" data-rota ="login">Entrar</a>
+                    <a href="#" class="link" data-rota ="login">Entrar</a>
                 </li>
                 <li class="registrar">
                     <a href="#" class="link" data-rota="registro" >Registrar</a>
--- src/Scripts/carregarPagina.js
+++ src/Scripts/carregarPagina.js
@@ -1,9 +1,9 @@
 export default async function carregarPagina(caminho, elemento){
     try {
         const resposta = await fetch(caminho) 
-        console.log(resposta)
+
         const conteudo = await resposta.text()
-        console.log(conteudo)
+
         document.getElementById(elemento).innerHTML = conteudo
     } catch (error) {
         console.log(
