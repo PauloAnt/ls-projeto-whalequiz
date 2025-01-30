675b328c4877688889da3838ef0d710e2943e2e6
PauloAnt <pauloabf235@gmail.com>
2025-01-29 21:44:33 -0300

    update: vercel e vite

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,16 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
+  assetsInclude: ["**/*.html"],
   server: {
     port: 8080,
+  },
+  build: {
+    outDir: 'dist',
   },
 
   plugins: [
     tailwindcss(),
   ],
-  
 })
