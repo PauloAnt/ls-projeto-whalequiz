186c8e7f2f3c3cd20a2e762351755aac34560e57
PauloAnt <pauloabf235@gmail.com>
2025-03-19 10:24:59 -0300

    update: configuração cors

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ app.use(express.json());
 app.use("/api", QuizRouter);
 app.use("/api", UserRouter);
 app.use("/api", QuestionRouter);
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 
 app.get("/status", (req, res) => {
     return res.status(200).json({
